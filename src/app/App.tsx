5cdd646c5ee603c6d06a0e7d880c460b5e246e64
dmitry <evselevd@mail.ru>
2023-07-02 23:57:41 +0300

    decomposed AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
 import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -14,12 +15,7 @@ const App = () => {
             <button onClick={toggleTheme}>toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
